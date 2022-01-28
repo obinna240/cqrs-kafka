5cf0342b39b001481303503892a782bd23dfbc70
obinna240 <obinna240@yahoo.co.uk>
2022-01-28 14:54:58 +0000

    Implemented Dispatchers

--- cqrs.core/src/main/java/com/ob/cqrs/core/commands/CommandHandlerMethod.java
+++ cqrs.core/src/main/java/com/ob/cqrs/core/commands/CommandHandlerMethod.java
@@ -1,5 +1,9 @@
 package com.ob.cqrs.core.commands;
 
+/**
+ * Functional interface which handles all types of commands
+ * @param <T>
+ */
 @FunctionalInterface
 public interface CommandHandlerMethod<T extends BaseCommand> {
     void handle(T command);
--- cqrs.core/src/main/java/com/ob/cqrs/core/infrastructure/CommandDispatcher.java
+++ cqrs.core/src/main/java/com/ob/cqrs/core/infrastructure/CommandDispatcher.java
@@ -3,6 +3,10 @@ package com.ob.cqrs.core.infrastructure;
 import com.ob.cqrs.core.commands.BaseCommand;
 import com.ob.cqrs.core.commands.CommandHandlerMethod;
 
+/**
+ * CommandDispatcher is essentially our mediator
+ * responsible for dispatching commands
+ */
 public interface CommandDispatcher {
 
     <T extends BaseCommand> void registerHandler(Class<T> type, CommandHandlerMethod<T> handler);
